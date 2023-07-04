9ad1e890b1e893276fd5a86d46f6ae5352fa2b85
Felipe Lobato <felipe.lobato@radixeng.com.br>
2023-07-04 13:28:51 -0300

    editable table for transactions

--- pages/2_edit.py
+++ pages/2_edit.py
@@ -8,12 +8,8 @@ class Edit():
         pass
 
     def page(self):
-        
         ts = Transactions()
-
         ts.editableTable(10);
-        # edited = st.data_editor(all_transactions, key="data_editor")
-        # st.json(st.session_state["data_editor"]["edited_rows"]);
         return
 
 Edit()
--- transactions.py
+++ transactions.py
@@ -1,3 +1,4 @@
+import json
 from database import Database
 import pandas as pd
 import streamlit as st
@@ -7,42 +8,40 @@ import datetime
 
 class Transactions():
     def __init__(self) -> None:
-        db = Database();
-        self.all_transactions = db.all_transactions;
-        self.all_categories = db.all_categories;
+        self.db = Database();
+        self.all_transactions = self.db.all_transactions;
+        self.all_categories = self.db.all_categories;
         self.allTransactionsDataframe();
         self.balance = self.income-self.expense;
+
+        self.defaultObject = {
+            "DT_PAYMENT":None,
+            "TX_TRANSACTION_DESCRIPTION":None,
+            "NU_TRANSACTION_VALUE":None,
+            "TX_CATEGORY_NAME":None,
+            "CD_CATEGORY_TYPE":None,
+        }
         pass
 
     def allTransactionsDataframe(self):
 
-        CD_CATEGORY_TYPE = {0: "🟢", 1: "🔻"}
+        CD_CATEGORY_TYPE = {0: "ENTRADA", 1: "SAÍDA"}
 
         all_transactions_df = pd.DataFrame(self.all_transactions)
         all_transactions_df["DT_PAYMENT"] = pd.to_datetime(all_transactions_df["DT_PAYMENT"])
-        all_transactions_df["NU_VALUE"] = pd.to_numeric(all_transactions_df["NU_VALUE"])
+        all_transactions_df["NU_TRANSACTION_VALUE"] = pd.to_numeric(all_transactions_df["NU_TRANSACTION_VALUE"])
         all_transactions_df.drop(columns=["ID_CREDIT_INVOICE_INFO"], inplace=True)
 
         all_categories_df =  pd.DataFrame(self.all_categories)
-        all_categories_df.drop(columns=["TX_DESCRIPTION"], inplace=True)
+        all_categories_df.drop(columns=["TX_CATEGORY_DESCRIPTION"], inplace=True)
 
         all_transactions_df = pd.merge(all_transactions_df, all_categories_df, on="ID_CATEGORY")
 
-        self.income = all_transactions_df[all_transactions_df["CD_CATEGORY_TYPE"] == 0]["NU_VALUE"].sum()
-        self.expense = all_transactions_df[all_transactions_df["CD_CATEGORY_TYPE"] == 1]["NU_VALUE"].sum()
-
-        # all_transactions_df.set_index("DT_PAYMENT", inplace=True)
-        # all_transactions_df.sort_index(inplace=True)
+        self.income = all_transactions_df[all_transactions_df["CD_CATEGORY_TYPE"] == 0]["NU_TRANSACTION_VALUE"].sum()
+        self.expense = all_transactions_df[all_transactions_df["CD_CATEGORY_TYPE"] == 1]["NU_TRANSACTION_VALUE"].sum()
 
         all_transactions_df["TIPO"] = all_transactions_df["CD_CATEGORY_TYPE"].map(CD_CATEGORY_TYPE)
-        #all_transactions_df['NU_VALUE'] = all_transactions_df['NU_VALUE'].map('R${:,.2f}'.format)
-
-        all_transactions_df.drop(columns=["CD_CATEGORY_TYPE", "ID_CATEGORY", "ID_USER"], inplace=True)
-
-        all_transactions_df.rename(columns={"TX_DESCRIPTION":"DESCRIÇÃO",
-                                #"NU_VALUE": "VALOR",
-                                "TX_NAME": "CATEGORIA"}, inplace=True)
-        #all_transactions_df.index.name='DT_PAYMENT'
+        all_transactions_df.drop(columns=["ID_CATEGORY", "ID_USER"], inplace=True)
 
         self.all_transactions_df = all_transactions_df;
 
@@ -57,31 +56,55 @@ class Transactions():
                         hide_index=True,
                         key="data_editor",
                         column_config={
-                            "CATEGORIA": st.column_config.SelectboxColumn(
-                                help="The category of the app",
+                            "TX_CATEGORY_NAME": st.column_config.SelectboxColumn(
                                 width="medium",
                                 required=True,
-                                options=map(lambda x: x["TX_NAME"], self.all_categories),
+                                options=map(lambda x: x["TX_CATEGORY_NAME"], self.all_categories),
                             ),
                             "DT_PAYMENT": st.column_config.DatetimeColumn(
                                 "DATA PAGAMENTO",
                                 format="DD/MM/YYYY",
+                                width="small", 
                             ),
-                            "DT_CREATED": st.column_config.DatetimeColumn(
-                                "DATA CRIAÇÃO",
-                                format="DD/MM/YYYY",
-                                disabled=True,
-                            ),
-                            "NU_VALUE": st.column_config.NumberColumn(
+                            "DT_CREATED": None,
+                            "NU_TRANSACTION_VALUE": st.column_config.NumberColumn(
                                 "VALOR",
                                 format="R$ %.2f",
-                            )
+                            ), 
+                            "TX_DESCRIPTION": st.column_config.TextColumn(
+                                        "DESCRIÇÃO",
+                                    ),
+                            "ID_TRANSACTION": None,
+                            "CD_CATEGORY_TYPE": None,
                         },)
 
-        st.write(st.session_state["data_editor"]["edited_rows"])
-        # for i in st.session_state["data_editor"]["edited_rows"]:
-        #     json = dict(dataframe.iloc[i].to_json()
-        #     id = json["ID_TRANSACTION"]
+        edited_rows = st.session_state["data_editor"]["edited_rows"]
+        
+        if len(edited_rows) > 0:
+            index_position = list(edited_rows.keys())[0];
+            object = json.loads(dataframe.iloc[index_position].to_json());
+
+            for k in self.defaultObject:
+                try:
+                    object[k] = edited_rows[list(edited_rows.keys())[0]][k]
+                except:
+                    pass
+
+            st.json(edited_rows)
+            st.json(object)
+            # self.db.insert_query(
+            #     f'''
+            #     UPDATE T_TRANSACTIONS
+            #     SET 
+            #         TX_TRANSACTION_DESCRIPTION = '{object["TX_TRANSACTION_DESCRIPTION"]}' 
+            #     WHERE ID_TRANSACTION = {object["ID_TRANSACTION"]}
+            #     '''
+            # )
+            # edited_rows.clear()
+            # st.experimental_rerun()
+
+
+        
 
     def balanceMetric(self):
         return st.metric('Saldo', self.formatMoney(self.balance), delta=12, delta_color="normal", help=None, label_visibility="visible");
